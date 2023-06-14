e89705389f6bda2cd84ed58472d036b0d66b0b31
SquidRings <chaudhryharis2@outlook.fr>
2023-06-14 19:47:56 +0200

    translate all and a todo in main.py

--- main.py
+++ main.py
@@ -6,53 +6,53 @@ client = docker.from_env()
 
 @app.route('/')
 def index():
-    # Obtenir la liste des conteneurs Docker actifs
+    # Get a list of active Docker containers
     containers = client.containers.list()
     return render_template('index.html', containers=containers)
 
 @app.route('/start_server', methods=['POST'])
 def start_server():
-    # Récupérer les informations du formulaire
+    # Retrieve form information
     game = request.form.get('game')
     port = request.form.get('port')
 
     if game == 'minecraft':
-        # Créer un nouveau conteneur pour le serveur Minecraft
+        # Create a new container for the Minecraft server
         container = client.containers.run('itzg/minecraft-server', ports={'25565/tcp': int(port)}, detach=True)
 
     elif game == 'ark':
-        # Créer un nouveau conteneur pour le serveur ARK: Survival Evolved
+        # Create a new container for the ARK: Survival Evolved server
         container = client.containers.run('troydo42/ark-docker', ports={'27015/tcp': int(port)}, detach=True)
 
     elif game == 'rust':
-        # Créer un nouveau conteneur pour le serveur Rust
+        # Create a new container for the Rust server
         container = client.containers.run('didstopia/rust-server', ports={'28015/tcp': int(port), '28016/tcp': int(port)}, detach=True)
 
     else:
-        return 'Jeu non pris en charge'
+        return 'Unsupported game'
 
-    return 'Serveur de jeu démarré avec succès !'
+    return 'Game server successfully started !'
 
 @app.route('/stop_server', methods=['POST'])
 def stop_server():
-    # Récupérer l'ID du conteneur à arrêter
+    # Retrieve the ID of the container to stop
     container_id = request.form.get('container_id')
 
-    # Arrêter et supprimer le conteneur Docker
+    # Stop and delete the Docker container
     container = client.containers.get(container_id)
     container.stop()
     container.remove()
 
-    return 'Serveur de jeu arrêté avec succès !'
+    return 'Game server shut down successfully !'
 
-
+# TODO: add a button to control all container with terminal ftp control and always show all containers run or not running
 @app.route('/container_control', methods=['POST'])
 def container_control():
-    # Obtenir la liste des conteneurs Docker actifs
+    # Get a list of active Docker containers
     containers = client.containers.list()
     return render_template('container_control.html', containers=containers)
 
-    # Récupérer l'action du bouton
+    # Retrieve button action
     action = request.form.get('action')
 
     container = client.containers.get(container_id)
@@ -61,7 +61,7 @@ def container_control():
     elif action == 'stop':
         container.stop()
 
-    return 'Action effectuée avec succès !'
+    return 'Action successfully completed !'
 
 if __name__ == '__main__':
     app.run(debug=True)
